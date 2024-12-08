45bde4662548ea2e08004ed50234b212f4fb9701
bharani2624 <bharani2624baru@gmail.com>
2024-12-08 23:29:57 +0530

    Made Some Changes

--- FileBrowser.sh
+++ FileBrowser.sh
@@ -5,8 +5,12 @@ clear
 echo "Welcome To ABD File Browser"
 echo "Current Working Directory:$PWD"
 echo "-------------------------------"
-ls -1 --group-directories-first | nl
+if [[ $show_hidden -eq 1 ]]; then
+content=($(ls -a1 --group-directories-first))
+else
+content=($(ls -1 --group-directories-first))
+fi
+all_content=${#content[@]}
 echo "-------------------------------"
-
 }
-init 
+init
